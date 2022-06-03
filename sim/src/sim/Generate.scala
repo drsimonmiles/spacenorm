2690ce9fb997729928402f6284bc3d1445f269ba
Simon Miles <drsimonmiles@gmail.com>
2022-06-03 20:01:40 +0100

    No corners for agents to get stuck in

--- sim/src/sim/Generate.scala
+++ sim/src/sim/Generate.scala
@@ -14,17 +14,34 @@ object Generate:
   randomly choosing obstacle and exit positions . */
   def newRunConfiguration(settings: Settings): Configuration = {
     import settings.*
-    val obstacleTopLefts: List[Position] =
-      List.fill(numberObstacles)(randomPosition(spaceWidth, spaceHeight))
+
+    def withinObstacle(obstacle: Position, point: Position): Boolean =
+      point.x >= obstacle.x && point.x < obstacle.x + obstacleSide &&
+        point.y >= obstacle.y && point.y < obstacle.y + obstacleSide
+
+    def obstacleBorder(topLeft: Position): List[Position] =
+      (for (d <- -1 until obstacleSide)
+        yield List(topLeft.move(d, -1), topLeft.move(d, obstacleSide), topLeft.move(-1, d), topLeft.move(obstacleSide, d)))
+        .toList.flatten
+
+    def overlaps(obstacles: List[Position], newObstacle: Position): Boolean =
+      obstacleBorder(newObstacle).exists(corner => obstacles.exists(existing => withinObstacle(existing, corner)))
+
+    def generateObstacles(soFar: List[Position]): List[Position] =
+      if (soFar.size == numberObstacles)
+        soFar
+      else {
+        val next = randomPosition(spaceWidth - obstacleSide - 1, spaceHeight - obstacleSide - 1).move(1, 1)
+        generateObstacles(if (overlaps(soFar, next)) soFar else next :: soFar)
+      }
+
+    val obstacleTopLefts: List[Position] = generateObstacles(Nil)
+      //List.fill(numberObstacles)(randomPosition(spaceWidth, spaceHeight))
 
     val possibleExits: List[Position] = {
       val xSides = for (x <- 0 until spaceWidth) yield List(Position(x, 0), Position(x, spaceHeight - 1))
       val ySides = for (y <- 0 until spaceHeight) yield List(Position(0, y), Position(spaceWidth - 1, y))
-      val oSides =
-        obstacleTopLefts.flatMap(pos =>
-          (for (d <- 0 until obstacleSide)
-            yield List(pos.move(d, -1), pos.move(d, obstacleSide), pos.move(-1, d), pos.move(obstacleSide, d)))
-        ) 
+      val oSides = obstacleTopLefts.map(obstacleBorder)
       (xSides ++ ySides ++ oSides).flatten.toList
     }
 
@@ -32,7 +49,8 @@ object Generate:
       possibleExits(Random.nextInt(possibleExits.size))
     }
 
-    Configuration(spaceWidth, spaceHeight, numberAgents, numberBehaviours, obstacleSide, threshold, distanceInfluence, maxMove, obstacleTopLefts, exits)
+    Configuration(spaceWidth, spaceHeight, numberAgents, numberBehaviours, obstacleSide, threshold,
+                  distanceInfluence, maxMove, obstacleTopLefts, exits)
   }
 
   private var nextID: Int = 0
