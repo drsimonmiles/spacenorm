20bb4bb503b6030a1edc8994c03085bfb625b512
Simon Miles <drsimonmiles@gmail.com>
2022-06-08 13:14:35 +0100

    Added mean utility, divergence metrics; Metrics calculated on behaviour counts not state

--- sim/src/sim/Metrics.scala
+++ sim/src/sim/Metrics.scala
@@ -1,5 +1,6 @@
 package sim
 
+import java.lang.Math.log
 import spacenorm.State
 
 /** Functionality to analyse simulation states to determine key aggregate metrics. */
@@ -9,11 +10,25 @@ object Metrics:
   def behaviourCounts(state: State): List[Int] =
     state.config.allBehaviours.map(behaviour => state.behaviour.values.count(_ == behaviour))
 
-  def converged(state: State): Boolean =
-    highestPrevalence(state) >= convergenceRatio
+  def converged(behaviourCounts: List[Int]): Boolean =
+    highestPrevalence(behaviourCounts) >= convergenceRatio
 
-  def highestPrevalence(state: State): Double =
-    behaviourCounts(state).max.toDouble / state.config.numberAgents
+  def diversity(behaviourCounts: List[Int]): Double = {
+    val total = behaviourCounts.sum.toDouble
+    val frequencies = behaviourCounts.map(_ / total)                              // p(a)
+    def log2(x: Double): Double = log(x) /log(2)                                  // log_2
+    val entropy = -frequencies.map(frequency => frequency * log2(frequency)).sum  // H(a)
+    
+    entropy / log2(behaviourCounts.size)
+  }
+
+  def highestPrevalence(behaviourCounts: List[Int]): Double =
+    behaviourCounts.max.toDouble / behaviourCounts.sum
+
+  def meanUtility(state: State): Double = {
+    val utilities = state.recentSuccess.values.toList
+    utilities.sum / utilities.size
+  }
 
   def neighbourhoodCorrelation(state: State): Double =
     val fractions = state.agents.map { agent =>
--- sim/src/sim/Result.scala
+++ sim/src/sim/Result.scala
@@ -1,15 +1,15 @@
 package sim
 
-import sim.Metrics.{behaviourCounts, neighbourhoodCorrelation}
+import sim.Metrics.{behaviourCounts, meanUtility, neighbourhoodCorrelation}
 import spacenorm.State
 
 /** A summary of key data about the state at the end of a given simulation tick. */
-final case class TickResult(tick: Int, prevalences: List[Int], neighbourhood: Double)
+final case class TickResult(tick: Int, prevalences: List[Int], neighbourhood: Double, meanUtility: Double)
 
 /** A summary of key data about the states across a simulation run. */
 final case class Result(run: Int, ticks: List[TickResult]):
   def addTick(tick: Int, state: State): Result =
-    copy(ticks = TickResult(tick, behaviourCounts(state), neighbourhoodCorrelation(state)) :: ticks)
+    copy(ticks = TickResult(tick, behaviourCounts(state), neighbourhoodCorrelation(state), meanUtility(state)) :: ticks)
 
 object Result:
   def apply(run: Int, initialState: State): Result =
